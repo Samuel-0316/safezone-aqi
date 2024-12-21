fe74cca19b7c85dfed469be93d6d6a20f1c00cac
uttamkn <uttamkn15@gmail.com>
2024-12-21 17:41:26 +0530

    feat: implement location input and design the home page

---
+++ lib/handlers.ts
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+
+export const handleLocationSubmit = async (formData: FormData) => {
+  const location = formData.get("location") as string;
+
+  if (location.trim() !== "") {
+    redirect(`/map?location=${location}`);
+  }
+};
