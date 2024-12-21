27607bbba5fb5d300c044f770edeb28f346a61a5
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-12-21 17:48:38 +0530

    feat: water quality api and types

---
+++ lib/meersens_api/WaterQuality.ts
@@ -0,0 +1,33 @@
+import { WaterQualityResponse } from "./WaterQualityTypes";
+
+export const getWaterQuality = async (
+  latitude: number,
+  longitude: number,
+): Promise<WaterQualityResponse> => {
+  if (!process.env.MEERSEENS_API_KEY) {
+    throw new Error("MEERSEENS_API_KEY is not set");
+  }
+  const response = await fetch(
+    `https://api.meerseens.com/environment/public/water/current?lat=${latitude}&lng=${longitude}&apikey=${process.env.MEERSEENS_API_KEY}`,
+  );
+  if (!response.ok) {
+    throw new Error(`Error ${response.status} ${response.statusText}`);
+  }
+  const data: WaterQualityResponse = await response.json();
+  if (!data) {
+    throw new Error("No data found for the given coordinates");
+  }
+  return data;
+};
+
+export const getWaterQualityMap = async (response: WaterQualityResponse) => {
+  if (response.pollutants === null) {
+    throw new Error("no pollutants found");
+  }
+  const res: Record<string, number> = {};
+  for (const key in response.pollutants) {
+    const pollutant = response.pollutants[key];
+    res[pollutant.name] = pollutant.value;
+  }
+  return res;
+};
---
+++ lib/meersens_api/WaterQualityTypes.ts
@@ -0,0 +1,41 @@
+export type WaterQualityResponse = {
+  found: boolean;
+  datetime: string; // ISO 8601 formatted date string
+  index: {
+    index_type: string;
+    index_name: string;
+    qualification: string;
+    icon: string | null;
+    color: string;
+    value: number;
+    main_pollutants: string[];
+  };
+  pollutants: Record<string, PollutantData>;
+  health_recommendations: {
+    all: string;
+    family: string;
+    sport: string;
+    pregnancy: string;
+    respiratory: string;
+    elderly: string;
+    cardiovascular: string;
+  };
+};
+
+type PollutantData = {
+  shortcode: string;
+  name: string;
+  unit: string;
+  found: boolean;
+  value: number;
+  confidence: number;
+  index: {
+    index_type: string;
+    index_name: string;
+    qualification: string;
+    icon: string | null;
+    color: string;
+    description: string;
+    value: number;
+  };
+};
