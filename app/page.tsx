96a8cdaf3e0be9b508d3c9d2c8ffedc1b4a636d7
uttamkn <uttamkn15@gmail.com>
2024-12-22 15:33:52 +0530

    fix: children props

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,9 @@ import BackgroundImg from "@/components/BackgroundImg";
 import TransparentScrollableContent from "@/components/transparent-scrollable-component";
 
 export default function Page() {
-  return <BackgroundImg children={<TransparentScrollableContent />} />;
+  return (
+    <BackgroundImg>
+      <TransparentScrollableContent />
+    </BackgroundImg>
+  );
 }
